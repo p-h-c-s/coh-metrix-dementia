751bcce8dfc44eec0caee5d426c3f1ff46263f82
Andre Cunha <andre.lv.cunha@gmail.com>
2015-02-23 14:00:23 -0300

    Added support for the Universal Tagset and for the OpenNLP tagger using this tagset.

--- coh/tools/tag/__init__.py
+++ coh/tools/tag/__init__.py
@@ -20,3 +20,4 @@ from __future__ import unicode_literals, print_function, division
 from coh.tools.tag.api import Tagger, TagSet
 from coh.tools.tag.opennlp import OpenNLPTagger
 from coh.tools.tag.macmorpho import OpenNLPMacMorphoTagger, MacMorphoTagSet
+from coh.tools.tag.universal import OpenNLPUniversalTagger, UniversalTagSet
