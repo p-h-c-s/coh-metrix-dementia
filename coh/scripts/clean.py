2a5aaed1faaac56c8f3162dbe8800f440add6e75
Andre Cunha <andre.lv.cunha@gmail.com>
2015-07-15 12:01:27 -0300

    Improved scripts for text cleaning and model training for LM.

--- coh/scripts/clean.py
+++ coh/scripts/clean.py
@@ -1,3 +1,5 @@
+"""Routines for cleaning text corpora, in preparation for SLM training."""
+
 # -*- coding: utf-8 -*-
 # Coh-Metrix-Dementia - Automatic text analysis and classification for dementia.
 # Copyright (C) 2014  Andre Luiz Verucci da Cunha
@@ -15,11 +17,80 @@
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import print_function
+
 import re
+import codecs
+import unicodedata
+from sys import argv, maxunicode
+from os.path import basename, splitext, dirname, join
 
-subs = [[r'``', '"'],
-        [r'\d+([,\.]\d*)?', '<NUM>'],
-        ]
+SUBS = [[r'``', '"'],  # Fix quotation marks
+        [r'\d+([,\.]\d*)?', '<NUM>'],  # Remove numbers
+        [r'\(.*?\)', ' '],  # Remove parenthetical clauses
+        [r'[^\u0000-\u00FF]', ' ']  # Remove invalid chars
+       ]
 
-for j in range(len(subs)):
-    subs[j][0] = re.compile(subs[j][0])
+for j in range(len(SUBS)):
+    SUBS[j][0] = re.compile(SUBS[j][0])
+
+# All unicode characters categorized as punctuation.
+EXCEPTIONS = ('%')
+PUNCT_TABLE = dict.fromkeys((i for i in range(maxunicode)
+                             if unicodedata.category(chr(i)).startswith('P')\
+                                     and chr(i) not in EXCEPTIONS),
+                            " ")
+
+MULTISPACES = re.compile(r'[ \t]+')
+
+
+def apply_subs(subs, string):
+    """Apply substitutions on a string."""
+
+    for left, right in subs:
+        string = left.sub(right, string)
+    return string
+
+
+def remove_punct(string):
+    """Remove punctuation marks."""
+
+    return string.translate(PUNCT_TABLE)
+
+
+def remove_multiple_spaces(string):
+    """Remove multiple spaces from a string."""
+
+    return MULTISPACES.sub(' ', string)
+
+
+def count_lines(filename):
+    """Count lines in file."""
+
+    with open(filename) as in_f:
+        count = 0
+        for _ in in_f:
+            count += 1
+        return count
+
+
+if __name__ == '__main__':
+    for path in argv[1:]:
+        print("Processing file", path)
+        total_lines = count_lines(path)
+
+        outdir = dirname(path)
+        base = basename(path)
+        name, ext = splitext(base)
+        outfilepath = join(outdir, name + '_clean' + ext)
+
+        with codecs.open(path, mode='r', encoding='utf-8') as infile,\
+                codecs.open(outfilepath, mode='w', encoding='utf-8') as outfile:
+            for lcount, line in enumerate(infile):
+                print('{0}/{1}'.format(lcount, total_lines), end="\r")
+                line = apply_subs(SUBS, line)
+                line = remove_punct(line)
+                line = remove_multiple_spaces(line)
+                outfile.write(line.strip())
+                outfile.write('\n')
+            print('')
--- coh/scripts/train_kenlm.sh
+++ coh/scripts/train_kenlm.sh
@@ -3,6 +3,6 @@
 KENLM_PATH=$HOME/Develop/kenlm/
 CORPORA_PATH=$HOME/Develop/corpora/
 
-cat $CORPORA_PATH/ngram_corpus_*.txt \
- | $KENLM_PATH/bin/lmplz -o 3 -S 80% -T /tmp \
+$KENLM_PATH/bin/lmplz -o 3 -S 50% -T /tmp \
+ < $CORPORA_PATH/ngram_corpus_clean.txt \
  | gzip > $CORPORA_PATH/corpus_3gram.arpa.gz
